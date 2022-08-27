bdb2946ed51ede92b38f21e2708755631729d911
Dmitriy Kuptsov <dmitriy.kuptsov@gmail.com>
2022-08-27 17:37:05 +0500

    Added score calculation

--- classifiers.py
+++ classifiers.py
@@ -1,49 +1,47 @@
 from sklearn.neural_network import MLPClassifier
-from sklearn.utils import resample
 from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA
 from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
 from sklearn import svm
+from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
+from sklearn.metrics import confusion_matrix
 import pandas as pd
 import numpy as np
-import sys
+import argparse
 
-df = pd.read_csv("ml2.pcap.csv")
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--file", help="features file")
+args = parser.parse_args()
+df = pd.read_csv(args.file)
 
 #df = df[["proto", "subproto", "bulk0", "bulk1", "bulk2", "bulk3", "client_bulksize_avg", "server_bulksize_avg"]]
 
-train, test = train_test_split(df, test_size=0.5)
-
+train, test = train_test_split(df, test_size=0.3)
 train_X = train.drop(["proto", "subproto"], axis=1)
-
 train_Y = train["proto"]
-
-#print(train_Y)
-
-#for row in train_Y:
-#    print(row)
 
 test_X = test.drop(["proto", "subproto"], axis=1)
 test_Y = test["proto"]
 
 # Neural network classifier
 
-ann = MLPClassifier(solver='sgd', max_iter=500, hidden_layer_sizes=(10, 5))
+ann = MLPClassifier(solver='sgd', hidden_layer_sizes=(10, 5))
 ann.fit(train_X, train_Y)
 pred_y = ann.predict(test_X)
 print("Artificial neural network accuracy:")
 print(np.sum(pred_y == test_Y) / len(test_Y))
-
+print('Precision: %.3f' % precision_score(test_Y, pred_y, average='micro'))
+print("Recall: %.3f" % recall_score(test_Y, pred_y, average='micro'))
 
 # Random forest
-
-from sklearn.ensemble import RandomForestClassifier
-clf = RandomForestClassifier(max_depth=4, random_state=0)
+clf = RandomForestClassifier(max_depth=10)
 clf.fit(train_X, train_Y)
 pred_y = clf.predict(test_X)
-
 print("Random forset accuracy:")
 print(np.sum(pred_y == test_Y)/len(test_Y))
+print('Precision: %.3f' % precision_score(test_Y, pred_y, average='micro'))
+print("Recall: %.3f" % recall_score(test_Y, pred_y, average='micro'))
 
 # Naive Bayes classifier
 model = GaussianNB()
@@ -51,17 +49,22 @@ model.fit(train_X, train_Y)
 pred_y = model.predict(test_X)
 print("Naive Bayes accuracy:")
 print(np.sum(pred_y == test_Y)/len(test_Y))
+print('Precision: %.3f' % precision_score(test_Y, pred_y, average='micro'))
+print("Recall: %.3f" % recall_score(test_Y, pred_y, average='micro'))
 
 # SVM
 clf = svm.SVC()
 clf.fit(train_X, train_Y)
-pred_y = model.predict(test_X)
+pred_y = clf.predict(test_X)
 print("SVM accuracy:")
 print(np.sum(pred_y == test_Y)/len(test_Y))
-
+print('Precision: %.3f' % precision_score(test_Y, pred_y, average='micro'))
+print("Recall: %.3f" % recall_score(test_Y, pred_y, average='micro'))
 # SVM
-clf = svm.SVC(kernel='rbf')
+clf = svm.SVC(kernel="rbf")
 clf.fit(train_X, train_Y)
-pred_y = model.predict(test_X)
+pred_y = clf.predict(test_X)
 print("SVM accuracy:")
 print(np.sum(pred_y == test_Y)/len(test_Y))
+print('Precision: %.3f' % precision_score(test_Y, pred_y, average='micro'))
+print("Recall: %.3f" % recall_score(test_Y, pred_y, average='micro'))
