4b497324c0b2396274991f6453c19216c8423ab1
Dmitriy Kuptsov <dmitriy.kuptsov@gmail.com>
2022-09-19 11:19:32 +0500

    Add files via upload

--- polynomial_regression.py
+++ polynomial_regression.py
@@ -7,39 +7,34 @@ import numpy as np
 from math import *
 
 def generate_sequence(n, step=1):
-	for i in arange(0, n, step):
-		yield sin(i)
+	for i in arange(-n, n, step):
+		yield -0.3*i*i*i*i*i+0.5*i*i*i*i+0.5*i*i*i+1*i*i+0.6*i
 
-gen = generate_sequence(10, 0.1)
+gen = generate_sequence(5, 0.1)
 seq = []
 for elem in gen:
-	elem += uniform(-3, 3)
+	elem += uniform(-50, 50)
 	seq.append(elem)
 
-plt.plot(arange(0, 10, 0.1), seq)
-plt.show()
-
-x = arange(0, 10, 0.1)
+x = arange(-5, 5, 0.1)
 x = np.array(x)
 bias = np.repeat(1, int(10/0.1))
 x1 = np.vstack((x, x*x))
 x2 = np.vstack((x1, x*x*x))
 x3 = np.vstack((x2, x*x*x*x))
-x4 = np.vstack((x3, bias))
-x = np.transpose(x4)
-
+x4 = np.vstack((x3, x*x*x*x*x))
+x5 = np.vstack((x4, x*x*x*x*x*x))
+x6 = np.vstack((x5, x*x*x*x*x*x*x))
+x7 = np.vstack((x6, bias))
+x = np.transpose(x7)
 y = seq
-
-theta = [0.7, 1, 10, 2, 1]
-
+theta = [0, 0, 0, 0, 0, 0, 0, 1]
 
 def cost_function(theta, x, y):
 	h = np.dot(x, theta)
 	n = np.shape(h)[0]
 	d = np.transpose(y) - h
 	return np.dot(np.transpose(d), d)/(2*n)
-
-print(cost_function(theta, x, y))
 
 def d_cost_function(theta, x, y):
 	h = np.dot(x, theta)
@@ -49,12 +44,12 @@ def d_cost_function(theta, x, y):
 
 print(d_cost_function(theta, x, y))
 
-epsilon = 1000000
+epsilon = inf
 iter = 0
 
-learning_rate = 0.00001
+learning_rate = 0.0000000000001
 
-while abs(epsilon) > 0.0001:
+while epsilon > 0.01:
 	c1 = cost_function(theta, x, y)
 	theta = theta - learning_rate * d_cost_function(theta, x, y)
 	print(theta)
@@ -65,25 +60,21 @@ while abs(epsilon) > 0.0001:
 	print("-----")
 
 
-#print(theta)
-#print(np.dot(np.transpose(theta), [[0, 1], [10, 1]]))
-
-
-x = arange(0, 10, 0.1)
+x = arange(-5, 5, 0.1)
 x = np.array(x)
-x0 = arange(0, 10, 0.1)
+x0 = arange(-5, 5, 0.1)
 
 bias = np.repeat(1, int(10/0.1))
 x1 = np.vstack((x, x*x))
 x2 = np.vstack((x1, x*x*x))
 x3 = np.vstack((x2, x*x*x*x))
-x4 = np.vstack((x3, bias))
-#x = np.transpose(x4)
-x = x4
+x4 = np.vstack((x3, x*x*x*x*x))
+x5 = np.vstack((x4, x*x*x*x*x*x))
+x6 = np.vstack((x5, x*x*x*x*x*x*x))
+x7 = np.vstack((x6, bias))
+x = x7
 
-plt.plot(arange(0, 10, 0.1), seq)
-print(x)
+plt.plot(arange(-5, 5, 0.1), seq, 'r+')
 plt.plot(x0, np.dot(np.transpose(theta), x))
-
 plt.show()
 
